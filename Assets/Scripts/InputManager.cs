78857b677841dc482e79e25891d51ad1906a41c1
mirmidion43 <69776783+mirmidion43@users.noreply.github.com>
2020-08-24 16:03:49 +1000

    finished 80%, changed input key to W instead of E

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -9,17 +9,26 @@ public class InputManager : MonoBehaviour
     void Start()
     {
         transArray = new Transform[2];
-        transArray[1] = GameObject.FindWithTag("Blue").transform;
-        transArray[2] = GameObject.FindWithTag("Red").transform;
+        Transform blue = GameObject.FindWithTag("Blue").transform;
+        Transform red = GameObject.FindWithTag("Red").transform;
+        transArray[0] = blue.transform;
+        transArray[1] = red.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.E))
+        if(Input.GetKeyDown(KeyCode.W))
         {
-            GameObject.FindWithTag("Red").transform.Rotate(0.0f, 0.0f, 45.0f);
-            GameObject.FindWithTag("Blue").transform.Rotate(0.0f, 0.0f, -45.0f);
+            transArray[0].transform.Rotate(0.0f, 0.0f, -45.0f);
+            transArray[1].transform.Rotate(0.0f, 0.0f, 45.0f);
+        }
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+            float temp = transArray[0].position.y;
+            transArray[0].transform.position = new Vector3(transArray[0].position.x, transArray[1].position.y, transArray[0].position.z);
+            transArray[1].transform.position = new Vector3(transArray[1].position.x, temp, transArray[1].position.z);
         }
     }
 }
